e79b7123ab89ae07616cd144747e4e488085613f
Vedad Bukva <vedadbukva8@gmail.com>
2020-06-03 17:08:03 +0200

    Postavljanje proxy-a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  path:'https://ada-backend.herokuapp.com/api'
+  path:'https://ada-backend.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  path:'https://ada-backend.herokuapp.com/api'
+  path:'https://ada-backend.herokuapp.com/'
 };
 
 /*
